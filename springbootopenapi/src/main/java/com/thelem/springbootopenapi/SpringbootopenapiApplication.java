4569ef1bb7addd69d45a4d4f72c17337bb1aad7a
lesouq <53365054+lesouq@users.noreply.github.com>
2020-07-01 13:53:23 +0200

    Add files via upload

---
+++ springbootopenapi/src/main/java/com/thelem/springbootopenapi/SpringbootopenapiApplication.java
@@ -0,0 +1,13 @@
+package com.thelem.springbootopenapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootopenapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootopenapiApplication.class, args);
+	}
+
+}
